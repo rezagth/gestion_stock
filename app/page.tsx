be4b0bbfb521033784a61ad3bd5e0ad00e78e572
rezagth <73990176+rezagth@users.noreply.github.com>
2025-01-02 16:12:28 +0100

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -212,7 +212,7 @@ export default function HomePage() {
   return (
     <div className="">
       {/* Hero Section */}
-      <div className="container mx-auto px-4 py-12">
+      <div className="container mx-auto px-4 py-4 ">
         <div className="text-center mb-12">
           <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-4">
             Gestion d'installation
--- components/licenses-navbar.tsx
+++ components/licenses-navbar.tsx
@@ -17,8 +17,9 @@ export default function LicensesNavBar() {
   if (!isLicensesSection) return null
 
   return (
-    <nav className="shadow-sm bg-gradient-to-r from-blue-600 to-blue-600 dark:from-slate-900 dark:to-slate-700">
-      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <nav className="
+     flex items-center top-20 left-0 right-0 z-50 w-full shadow-sm bg-gradient-to-r from-white to-blue-600 text-blue-600 dark:from-slate-900 dark:to-slate-700">
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-16">
         <div className="flex justify-center h-12">
           <div className="hidden sm:flex sm:space-x-8">
             {navItems.map((item) => {
@@ -29,10 +30,8 @@ export default function LicensesNavBar() {
                   href={item.href}
                   className={`
                     ${isActive
-                      // en dark mode mettre en bleu
-                      
-                      ? 'border-b-2 border-white text-white '
-                      : 'border-transparent text-blue-50 hover:text-white  '
+                      ? 'border-b-2 border-white text-blue-600'
+                      : 'border-transparent text-blue-50 hover:text-white'
                     } 
                     inline-flex items-center px-3 pt-1 text-sm font-medium
                     transition-colors duration-200 gap-2 hover:bg-blue-500/20 dark:hover:bg-slate-800
@@ -59,10 +58,9 @@ export default function LicensesNavBar() {
                 className={`
                   ${isActive
                     ? 'bg-blue-700 text-white'
-                    : 'text-blue-50 hover:bg-blue-600'
+                    : 'text-blue-50 hover:bg-blue-500/20 hover:text-white'
                   }
                   flex flex-col items-center px-3 py-2 rounded-md text-xs font-medium
-                  transition-colors duration-200 flex-1 mx-1
                 `}
               >
                 {item.icon}
